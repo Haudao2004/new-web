011db4a9537bc339dcc7caec719d3210051e742e
Dao Hau <Daohau300404@gmail.com>
2023-07-05 22:54:13 +0700

    a new project

---
+++ CTDL/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    // int text=20;
+    // (text>22)? printf("dao") : printf("sup");
+    int i[2][3]={{2,3,4}, {4,6,7}};
+    printf("%d\n", i[1][1]);
+
+    return 0;
+}
+
